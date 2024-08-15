2a0ddd7db18051980ab0adced30978c9e8ac1921
Zanzibar <zanzisworld42@gmail.com>
2024-08-15 09:41:23 -0700

    Finish component that lists section content.

---
+++ components/content/SectionContents.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="page in sectionPages" :key="page._path" class="p-2 my-2 border border-blue-800 hover:bg-blue-300">
+      <NuxtLink :to="page._path">{{ page.title }}</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { navigation } = useContent()
+const props = defineProps(['sectionName'])
+
+console.log(navigation.value);
+
+const pagesListIndex = navigation.value.findIndex(item => item.title === props.sectionName)
+const sectionPages = computed(() => {
+  return navigation.value[pagesListIndex].children.filter(page => page.title !== props.sectionName)
+})
+</script>
+
+<style></style>
